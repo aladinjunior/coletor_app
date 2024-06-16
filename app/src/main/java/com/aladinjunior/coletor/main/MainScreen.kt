9341b0c0158e9ea82e2a6ad41a1a47bdc28340dc
aladin <aladinjr013@icloud.com>
2024-06-15 23:12:20 -0300

    Modified main screen

--- app/src/main/java/com/aladinjunior/coletor/main/MainScreen.kt
+++ app/src/main/java/com/aladinjunior/coletor/main/MainScreen.kt
@@ -1,7 +1,9 @@
 package com.aladinjunior.coletor.main
 
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -13,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -33,7 +36,11 @@ fun MainScreen(
 
     Column {
         AppTabRow()
-        CameraPreview()
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center){
+            CameraPreview()
+
+        }
     }
 
 
